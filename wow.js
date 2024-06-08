776f7cce14894c038e6b4fc153842dcc7c8b2b0e
Leonardo Costa <costaleo93@gmail.com>
2024-06-08 01:38:07 -0400

    this is just a sample test

--- wow.js
+++ wow.js
@@ -2,3 +2,7 @@
 
 console.log('yea this is cool right');
 
+//cool now im in the test branch now does it make a differecne
+//i dont think it does to be honest with you. how do you feel about that. 
+//the keys here feel faster and smoother in a sense. why not keep it.
+//are you going to switch it out. i cant say im going to change it but i will consider it 
