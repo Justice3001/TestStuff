e6c00f96a6d33f416cb19d8e4b43d1da99fdef89
Leonardo Costa <costaleo93@gmail.com>
2024-06-10 22:51:57 -0400

    nothing here really

---
+++ here.js
@@ -0,0 +1,6 @@
+//testing some bash commands here...
+
+
+console.log('hello from the here.js');
+
+//nothing to see here. 
--- wow.js
+++ wow.js
@@ -1,8 +1,20 @@
 //just a quick test to git stuff
 
+let start=Date.now();
+
 console.log('yea this is cool right');
 
 //cool now im in the test branch now does it make a differecne
 //i dont think it does to be honest with you. how do you feel about that. 
 //the keys here feel faster and smoother in a sense. why not keep it.
 //are you going to switch it out. i cant say im going to change it but i will consider it 
+
+console.log("just some more text here...");
+
+
+
+let end=Date.now();
+
+let duration= (end-start);
+
+console.log("this program took",duration,"seconds to complete.");
